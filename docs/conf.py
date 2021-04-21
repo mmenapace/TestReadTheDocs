7ed82b9bb63266b5e891dbe9d3bda9da81b86c07
Marco <m.menapace@fadeout.it>
2021-04-21 15:40:14 +0200

    seems hacky? it is ! but is works?

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,9 @@ html_static_path = ['_static']
 
 import os
 
-os.system("echo Hello from the other side!")
-os.system("pwd")
-os.system("ls")
+
+
+os.system("echo --- HOT FIX for Javasphinx library ---")
+os.system("echo --- overwriting domain.py with the fixed version ---")
+os.system("mv domain.py /home/docs/checkouts/readthedocs.org/user_builds/testreadthedocsbymena/envs/latest/lib/python3.7/site-packages/javasphinx/domain.py")
+
